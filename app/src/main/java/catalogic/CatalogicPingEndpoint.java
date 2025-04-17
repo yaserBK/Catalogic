23ab8fee992dcef7e30d71a683cb09793166d9eb
Yaser B.K. <myaserbk@gmail.com>
2025-04-17 20:58:50 +0100

    test push

--- app/src/main/java/catalogic/CatalogicPingEndpoint.java
+++ app/src/main/java/catalogic/CatalogicPingEndpoint.java
@@ -1,7 +1,6 @@
 package catalogic;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +8,7 @@ class CatalogicPingEndpoint {
 
     @GetMapping("/ping")
     public String ping() {
-        return "PONG! Catalogic is up...";
+        return "PONG! Catalogic is RUNNING...";
     }
     
 
